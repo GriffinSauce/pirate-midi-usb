2c1197ea2b029ed4d4c1e15bf092f3304ca7bff5
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-06-06 15:49:18 +0200

    Document things a bit in test.js

--- src/test.js
+++ src/test.js
@@ -3,9 +3,18 @@ import { getDevices } from "./index.js";
 /**
  * TEST PAD
  */
+
+// Get all connected PM devices
 const devices = await getDevices();
 
-console.log(
-  "devices",
-  devices.map((device) => device.deviceInfo)
+// deviceInfo is already populated so you can easily select a target
+const bridge6 = devices.find(
+  ({ deviceInfo }) => deviceInfo.deviceModel === "Bridge6"
 );
+
+console.log("bridge6", bridge6.deviceInfo);
+
+// Commands are bound to simple functions and return a promise
+const globalSettings = await bridge6.getGlobalSettings();
+
+await bridge6.goToBank(1);
