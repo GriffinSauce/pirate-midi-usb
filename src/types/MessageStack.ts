65d0cd930af2cdf6085cb3e7fa2831a97a6e2646
Joris Griffioen <jorisjgriffioen@gmail.com>
2024-05-10 22:36:14 +0200

    chore: Update types & fixtures

--- src/types/MessageStack.ts
+++ src/types/MessageStack.ts
@@ -14,4 +14,6 @@ export interface AuxMessages {
 export interface AuxMessageStacks {
 	pressMessages: MessageStack;
 	holdMessages: MessageStack;
+	toggleOnMessages: MessageStack;
+	toggleOffMessages: MessageStack;
 }
