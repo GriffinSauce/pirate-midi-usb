0f57637f81b2309ac7e88dbd318fa691d16b565f
konn <konn.sim@gmail.com>
2025-05-13 22:30:25 +1000

    feat: refactor device descriptor download script to separate bridge and click descriptors

--- scripts/download-device-descriptors.ts
+++ scripts/download-device-descriptors.ts
@@ -3,31 +3,40 @@ import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
 
-const url =
+const bridgeUrl =
 	'https://raw.githubusercontent.com/Pirate-MIDI/device-descriptors-api/main/device-descriptors/bridge-descriptors.json';
+const clickUrl =
+	'https://raw.githubusercontent.com/Pirate-MIDI/device-descriptors-api/main/device-descriptors/click-descriptors.json';
 
 const outDir = path.join(__dirname, '../src/data');
-const outputPath = path.join(outDir, 'deviceDescriptors.ts');
+const bridgeOutputPath = path.join(outDir, 'bridgeDescriptors.ts');
+const clickOutputPath = path.join(outDir, 'clickDescriptors.ts');
 
 const banner = `
 /**
- * NOTE: this is downloaded and built from source on install, DO NOT modify. 
+ * NOTE: this is downloaded and built from source on install, DO NOT modify.
  * https://github.com/Pirate-MIDI/device-descriptors-api/blob/main/device-descriptors/bridge-descriptors.json
+ * https://github.com/Pirate-MIDI/device-descriptors-api/blob/main/device-descriptors/click-descriptors.json
  */
 `.trim();
 
 void (async () => {
-	const response = await fetch(url);
-	const data = await response.text();
+	const bridgeResponse = await fetch(bridgeUrl);
+	const bridgeData = await bridgeResponse.text();
+	const clickResponse = await fetch(clickUrl);
+	const clickData = await clickResponse.text();
 
-	const content = `${banner}\n     export const deviceDescriptors = ${data}`;
+	const bridgeContent = `${banner}\n     export const bridgeDescriptors = ${bridgeData}`;
+	const clickContent = `${banner}\n     export const clickDescriptors = ${clickData}`;
 
 	if (!fs.existsSync(outDir)) {
 		fs.mkdirSync(outDir);
 	}
-	fs.writeFileSync(outputPath, content);
+	fs.writeFileSync(bridgeOutputPath, bridgeContent);
+	fs.writeFileSync(clickOutputPath, clickContent);
 
 	execSync('npm run format');
 
-	console.info(`Device descriptors downloaded to ${outputPath}`);
+	console.info(`Bridge descriptors downloaded to ${bridgeOutputPath}`);
+	console.info(`Click descriptors downloaded to ${clickOutputPath}`);
 })();
